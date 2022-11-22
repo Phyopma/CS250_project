aed6537e91c0e944d772a197883d674453059594
Phyopma <phyopyaemaung1@gmail.com>
2022-11-21 20:13:05 -0800

    Project created

---
+++ Course.h
@@ -0,0 +1,25 @@
+/*
+(name header)
+*/
+#ifndef COURSE_H
+#define COURSE_H
+#include <string>
+#include <set>
+
+class Course
+{
+public:
+// Constructors
+// Accessor functions
+// Mutator functions
+// Destructor
+private:
+    int courseNumber;
+    std::string courseName;
+    int courseUnits;
+    std::set<int> prereqs;
+    static const std::string PREFIX;
+};
+#endif
+
+
