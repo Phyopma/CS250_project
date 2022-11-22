35be6c8fb15fd37261e7de02f922c97042e571ac
Phyopma <phyopyaemaung1@gmail.com>
2022-11-21 20:55:10 -0800

    Course.h finished!

--- Course.h
+++ Course.h
@@ -3,16 +3,52 @@
 */
 #ifndef COURSE_H
 #define COURSE_H
+
 #include <string>
 #include <set>
 
-class Course
-{
+class Course {
 public:
 // Constructors
+
+// default constructor
+    Course() : courseNumber(0),
+               courseName("N/A"),
+               courseUnits(0) {}
+
+// overload constructor
+    Course(int otherCourseNumber, const std::string &otherCourseName,
+           int otherCourseUnits, std::set<int> &otherPrereqs) :
+            courseNumber(otherCourseNumber),
+            courseName(otherCourseName),
+            courseUnits(otherCourseUnits),
+            prereqs(otherPrereqs) {}
+
 // Accessor functions
+    std::string &getCourseName() const;
+
+    int getCourseNumber() const;
+
+    int getCourseUnits() const;
+
+    std::set<int> &getCoursePrereqs() const;
+
+    Course &getCourse() const;
+
+    static std::string getPrefix();
+
 // Mutator functions
+    void setCourseName(std::string &);
+
+    void setCourseNumber(int);
+
+    void setCourseUnits(int);
+
+    void setPrereqs(int[], int);
+
 // Destructor
+    ~Course();
+
 private:
     int courseNumber;
     std::string courseName;
@@ -20,6 +56,7 @@ private:
     std::set<int> prereqs;
     static const std::string PREFIX;
 };
+
 #endif
 
 
