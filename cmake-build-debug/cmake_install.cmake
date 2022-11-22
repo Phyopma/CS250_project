d082dff6b8804254fc168093714609fdff52b121
Phyopma <phyopyaemaung1@gmail.com>
2022-11-21 22:05:07 -0800

    Google test added!

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/phyopyae/Desktop/CS250_project
+# Install script for directory: /Users/phyopyae/CLionProjects/CS250_project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/phyopyae/Desktop/CS250_project/cmake-build-debug/Google_tests/cmake_install.cmake")
+  include("/Users/phyopyae/CLionProjects/CS250_project/cmake-build-debug/Google_tests/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/phyopyae/Desktop/CS250_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/phyopyae/CLionProjects/CS250_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
