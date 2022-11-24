3ede9518c45708967512add85c547b0a82281da0
Phyopma <phyopyaemaung1@gmail.com>
2022-11-24 10:28:23 -0800

    test refractored

--- Test/test.cpp
+++ Test/test.cpp
@@ -12,7 +12,7 @@ TEST_CASE("default constructor test") {
 }
 
 //
-TEST_CASE("constructor test") {
+TEST_CASE("constructor and getter test") {
     set<int> s = {1, 2};
     Course c(250, "C++ Programming 2", 3, s);
     CHECK_EQ(c.getCoursePrereqs().size(), 2);
@@ -20,65 +20,39 @@ TEST_CASE("constructor test") {
     CHECK_EQ(c.getCourseName(), "C++ Programming 2");
     CHECK_EQ(c.getCourseUnits(), 3);
     CHECK_EQ(c.getCoursePrereqs(), s);
-}
-
-TEST_CASE("getCourseName test") {
-    set<int> s = {1, 2};
-    Course c(250, "C++ Programming 2", 3, s);
-    CHECK_EQ(c.getCourseName(), "C++ Programming 2");
-}
-
-TEST_CASE("getCourseNumber test") {
-    set<int> s = {1, 2};
-    Course c(250, "C++ Programming 2", 3, s);
-    CHECK_EQ(c.getCourseNumber(), 250);
-}
-
-TEST_CASE("getCourseUnits test") {
-    set<int> s = {1, 2};
-    Course c(250, "C++ Programming 2", 3, s);
-    CHECK_EQ(c.getCourseUnits(), 3);
-}
-
-TEST_CASE("getCoursePrereqs test") {
-    set<int> s = {1, 2};
-    Course c(250, "C++ Programming 2", 3, s);
-    CHECK_EQ(c.getCoursePrereqs(), s);
-}
-
-TEST_CASE("getCourse test") {
-    set<int> s = {1, 2};
-    Course c(250, "C++ Programming 2", 3, s);
     CHECK_EQ(&c, &(c.getCourse()));
-
 }
 
-TEST_CASE("getPrefix test") {
+TEST_CASE("static method test") {
     CHECK_EQ(Course::getPrefix(), "CS A");
 }
 
-TEST_CASE("setCourseName test") {
+TEST_CASE("setter methods test") {
     Course c;
     c.setCourseName("Programming");
     CHECK_EQ(c.getCourseName(), "Programming");
-}
-
-TEST_CASE("setCourseNumber test") {
-    Course c;
     c.setCourseNumber(250);
     CHECK_EQ(c.getCourseNumber(), 250);
-}
-
-TEST_CASE("setCourseUnits test") {
-    Course c;
     c.setCourseUnits(4);
     CHECK_EQ(c.getCourseUnits(), 4);
 }
 
-TEST_CASE("setCoursePrereqs test") {
-    int array[] = {170, 150, 250};
-    set<int> s = {170, 150, 250};
+TEST_CASE("setPreReqs test") {
     Course c;
-    c.setPrereqs(array, 3);
-    CHECK_EQ(c.getCoursePrereqs(), s);
+    SUBCASE("Empty set") {
+        int array[] = {170, 150, 250};
+        set<int> s = {170, 150, 250};
+        c.setPrereqs(array, 3);
+        CHECK_EQ(c.getCoursePrereqs(), s);
+    }
+    SUBCASE("Not empty set") {
+        int array1[] = {45, 12, 50, 93};
+        set<int> s1 = {45, 12, 50, 93};
+        c.setPrereqs(array1, 4);
+        REQUIRE_GT(c.getCoursePrereqs().size(), 3);
+        CHECK_EQ(c.getCoursePrereqs(), s1);
+    }
+
 }
+
+
