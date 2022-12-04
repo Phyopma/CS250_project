fe90c0fe27ff3e723ae494ad884b9c182f7bf396
vonne-9 <yuyuhlaing.chen@gmail.com>
2022-12-03 17:46:45 -0800

    call getPrefix

--- MainForPartA.cpp
+++ MainForPartA.cpp
@@ -28,13 +28,14 @@ int main() {
 // Create a course named "java" using the default constructor
     Course java;
 // Print the course by using all the get functions.
-    cout << "java: " << "CS A" << java.getCourseNumber() << " ";
-    cout << java.getCourseName() << " ";
-    cout << java.getCourseUnits() << " ";
+    cout << "java: " << Course::getPrefix() << java.getCourseNumber();
+    cout << " - " << java.getCourseName();
+    cout << " (" << java.getCourseUnits() << " units)";
 // To print the prerequisites, use a range-based loop
 // (note that there is no need to create a set; you
 // can call the getCoursePrereqs function in the loop header,
 // because it would have the same running time as creating a new set.
+    cout << " Prerequisites: ";
     for (int i: java.getCoursePrereqs()) {
         cout << i << " ";
     }
@@ -53,9 +54,10 @@ int main() {
 // Call function setPrereqs to pass the array and the length.
 // Print the course as you did above, to make sure the course
 // has all new values.
-    cout << "\njava: " << "CS A" << java.getCourseNumber() << " ";
-    cout << java.getCourseName() << " ";
-    cout << java.getCourseUnits() << " ";
+    cout << "\njava: " << Course::getPrefix() << java.getCourseNumber();
+    cout << " - " << java.getCourseName();
+    cout << " (" << java.getCourseUnits() << " units)";
+    cout << " Prerequisites: ";
     for (int i: java.getCoursePrereqs()) {
         cout << i << " ";
     }
@@ -65,9 +67,10 @@ int main() {
 // You can use as a fourth parameter the call: set<int>{150, 170}
     Course cpp = {250, "C++ 2", 4, set<int>{150, 170}};
     // Print the "cpp" course.
-    cout << "\ncpp: " << "CS A" << cpp.getCourseNumber() << " ";
-    cout << cpp.getCourseName() << " ";
-    cout << cpp.getCourseUnits() << " ";
+    cout << "\ncpp: " << Course::getPrefix << cpp.getCourseNumber();
+    cout << " - " << cpp.getCourseName();
+    cout << " (" << cpp.getCourseUnits() << " units)";
+    cout << " Prerequisites: ";
     for (int i: cpp.getCoursePrereqs()) {
         cout << i << " ";
     }
@@ -80,9 +83,10 @@ int main() {
     Course aCourse = java.getCourse();
 // Print "aCourse" to make it is an exact copy of "java".
     cout << "\naCourse: ";
-    cout << "CS A" << aCourse.getCourseNumber() << " ";
-    cout << aCourse.getCourseName() << " ";
-    cout << aCourse.getCourseUnits() << " ";
+    cout << Course::getPrefix() << aCourse.getCourseNumber();
+    cout << " - " << aCourse.getCourseName();
+    cout << " (" << aCourse.getCourseUnits() << " units)";
+    cout << " Prerequisites: ";
     for (int i: aCourse.getCoursePrereqs()) {
         cout << i << " ";
     }
@@ -97,17 +101,19 @@ int main() {
     java.setPrereqs(arr, 2);
 // Print "java".
     cout << "\njava: ";
-    cout << "CS A" << java.getCourseNumber() << " ";
-    cout << java.getCourseName() << " ";
-    cout << java.getCourseUnits() << " ";
+    cout << Course::getPrefix() << java.getCourseNumber();
+    cout << " - " << java.getCourseName();
+    cout << " (" << java.getCourseUnits() << " units)";
+    cout << " Prerequisites: ";
     for (int i: java.getCoursePrereqs()) {
         cout << i << " ";
     }
 // Print "aCourse", to make sure that it is still the same.
     cout << "\naCourse: ";
-    cout << "CS A" << aCourse.getCourseNumber() << " ";
-    cout << aCourse.getCourseName() << " ";
-    cout << aCourse.getCourseUnits() << " ";
+    cout << Course::getPrefix() << aCourse.getCourseNumber();
+    cout << " - " << aCourse.getCourseName();
+    cout << " (" << aCourse.getCourseUnits() << " units)";
+    cout << " Prerequisites: ";
     for (int i: aCourse.getCoursePrereqs()) {
         cout << i << " ";
     }
